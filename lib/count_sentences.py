c0d8ff76f7a7ae146fffed578666d50308c71e27
MBURU <wawerumburu157@gmail.com>
2024-12-17 22:45:59 +0300

    changes

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if isinstance(value, str): 
+            self.value = value
+        else:
+            raise ValueError("Value must be a string")
+
+    def is_sentence(self):
+        """Returns True if the string ends with a period."""
+        return self.value.endswith('.')
+
+    def is_question(self):
+        """Returns True if the string ends with a question mark."""
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        """Returns True if the string ends with an exclamation mark."""
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        """Counts the number of sentences in the string."""
+        
+        import re
+
+        
+        sentences = re.split(r'[.?!]', self.value)
+        
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
+
+
+string = MyString("This is a string! It has three sentences. Right?")
+print(string.count_sentences()) 
+
